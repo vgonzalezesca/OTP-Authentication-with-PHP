d6a6e3f9a4ae6a791f7ac51001ede9db1baeec4f
unknown <khalilinassim85@gmail.com>
2025-09-06 17:34:43 +0330

    signin action added

---
+++ action/sign-in.php
@@ -0,0 +1,37 @@
+<?php 
+require_once('../config/loader.php');
+
+if(isset($_POST["signin"])){
+    try{
+        //parametrs
+        $key = $_POST['key'];
+        $password = $_POST['password'];
+        
+        // sql
+        $query = "SELECT * FROM `users` WHERE (username= :key OR mobile = :key OR email = :key) AND (password = :password) LIMIT 1";
+        
+        // stmt
+        $stmt = $conn->prepare($query);
+        
+        // bind
+        $stmt->bindValue(":key", $key);
+        $stmt->bindValue(":password", $password);
+        
+        // exe
+        $stmt->execute();
+
+        $hasUser = $stmt->rowCount();
+        
+        if($hasUser){
+           header('Location: ../index.php?loginned=ok');
+        }else{
+           header('Location: ../index.php?notuser=ok');
+        }
+
+        // echo "Created Account!";
+        // header('Location: ../index.php');
+    }catch(PDOException $e){
+            echo "Your error is : " . $e->getMessage();
+    }
+
+}
